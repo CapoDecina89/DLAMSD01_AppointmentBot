3e74bdd2988842be11c110dca04a646e6a425a0a
capodecina89 <benjamin.grunow@iubh-fernstudium.de>
2022-07-20 12:15:01 +0200

    Initial Commit

---
+++ DLAMSD01_AppointmentBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DLAMSD01_AppointmentBot
+//
+//  Created by Benjamin Grunow on 20.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
