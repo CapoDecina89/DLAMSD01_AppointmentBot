13d2b751e795a25e809b207d4adf72048e1b5af7
capodecina89 <benjamin.grunow@iubh-fernstudium.de>
2022-09-15 13:10:41 +0200

    Einfügen der Pfade mit string.contains

--- DLAMSD01_AppointmentBot/ConversationDelegate.swift
+++ DLAMSD01_AppointmentBot/ConversationDelegate.swift
@@ -18,26 +18,42 @@ class ConversationDelegate {
     
     /// Creates an answer message in response to a question.
     func responseTo(question: String) -> String {
-        let lowerQuestion = question.lowercased()
         
         //review: argument label
         if checkLanguage(question: question) != NLLanguage.english{
             return "Please ask me in English. 🇬🇧"
+        //refacturate: interpretation of text as separate function, that is called by responseTo()
+        }else {
+            return interpret(question: question)
         }
+    }
+    
+    
+    func interpret(question:String) -> String {
+        let lowerQuestion = question.lowercased()
+        
         //Path appointment
-        else if lowerQuestion.contains("appointment") {
-            return "Sure. I can help you with your appointment./nWould you like to have info on your next appointment, make a new appointment or reschedule an existing?"
+        if lowerQuestion.contains("appointment") {
+            return "Sure. I can help you with your appointment.\nWould you like to have info on your next appointment, make a new appointment or reschedule an existing?"
             //insert cases: next, new and reschedule
+        }
+        //Path next appointment
+        else if lowerQuestion.contains("next") {
+            //DataSource for appointments should be added later. For prototyp-use today + 3 days
+            return "Your next appointment is due \(dateFormatter.string(from: Date(timeIntervalSinceNow: 259200)))."
         }
         //Path opening hours
         else if lowerQuestion.contains("open") {
-            return "We are open:/nMo - Fr 0800 - 1700"
+            return "We are open:\nMo - Fr 08:00 - 17:00"
         }
         //Path Direction
         else if lowerQuestion.hasPrefix("where") || lowerQuestion.contains("find") {
             return "One Apple Park Way, Cupertino, CA 95014"
-        }else {
-            return "Would you like to contact our reception?"
+        }
+        //Path misc
+        else {
+            return "Surely our reception can answer this question. Please contact us.\ntel:+4901234567889"
         }
     }
 }
+
